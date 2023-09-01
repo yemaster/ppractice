3df1fd43f841df903c4265bd5bac171c2b91ca6c
Yemaster <30290923+yemaster@users.noreply.github.com>
2023-09-01 21:53:36 +0800

    Add files via upload

---
+++ fetchProblem2.py
@@ -0,0 +1,42 @@
+import json
+oriText = []
+with open("problem2.txt", encoding="utf-8") as f:
+    oriText = f.read().split("\n")
+
+num = ["①", "②", "③", "④", "⑤", "⑥", "⑦", "⑧", "⑨"]
+chs = ["A", "B", "C", "D", "E"]
+problems = []
+now = {
+    "content": [],
+    "choices": [],
+    "answer": 0
+}
+for i in oriText:
+    i = i.strip()
+    if len(i) == 0:
+        continue
+    d = i.split(".")
+    if len(d) > 1 and not d[0].upper() in chs:
+        problems.append(now)
+        now = {
+            "content": "",
+            "choices": [],
+            "answer": 0
+        }
+    if len(i) > 0 and i[-2] in chs:
+        now["answer"] = chs.index(i[-2])
+        i = i[:-3]
+        d = i.split(".")
+    if len(i) > 0 and i[0].upper() in chs and len(d) > 1:
+        now["choices"].append(d[1])
+    else:
+        if len(now["choices"]) > 0:
+            now["choices"][-1] += d[-1]
+        else:
+            if len(i) > 0 and i[0] in num:
+                now["content"] += "\n"
+            now["content"] += d[-1]
+problems.append(now)
+print(len(problems[1:]))
+with open("problems2.json", "w", encoding="utf-8") as f:
+    json.dump(problems[1:], f)
